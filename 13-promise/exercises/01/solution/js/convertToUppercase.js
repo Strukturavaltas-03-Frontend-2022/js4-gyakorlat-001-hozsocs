e8371f9d73eb64ebe2a43f0025bc19f7284189c0
hozsocs <hozsocs@gmail.com>
2022-09-07 07:48:44 +0200

    Az error üzenetekkel bajban vagyok

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,20 @@
+/* 1. Írj egy függvényt, `convertToUppercase()` néven, mely paraméterként kap egy tömböt.
+ A függvény visszatérési érték egy Promise.
+Amennyiben a tömb minden eleme string, és sikeresen nagybetűssé alakíthatók a Promise
+ resolved lesz és visszaad egy új tömböt, ami az eredeti tömb összes elemét nagybetűssé
+  alakítva tartalmazza.
+Ellenkező esetben a Promise rejected lesz, a visszatérési érték a következő string:
+'Error: Not all items in the array are strings!' */
+
+const convertToUppercase = ([...string]) => {
+  const question = (string.every((item) => typeof item === 'string'));
+  return new Promise((resolve, reject) => {
+    if (question) { resolve(string.map((f) => f.toUpperCase())); } else { reject('Error: Not all items in the array are strings!'); }
+  }).then(
+    (value) => value,
+    (err) => err,
+  );
+};
+
+// console.log(convertToUppercase(['qeqwe', 'weqwe', 'erew', 'erwe', 11]));
 export default convertToUppercase;
